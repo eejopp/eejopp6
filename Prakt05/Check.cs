b3601b963d227e7d7bac4493b92b9bc637018a56
eejopp <144217724+eejopp@users.noreply.github.com>
2023-10-25 12:01:50 +0300

    Add files via upload

---
+++ Prakt05/Check.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prakt05
+{
+    class Proper
+    {
+        //член класса
+        private string text;
+        private int price;
+
+        //конструктор
+        public Proper(string _text, int _price)
+        {
+            text = _text;
+            if (_price < 0) {
+                throw new ArgumentOutOfRangeException("У свойства не может быть отрицательной цены");
+            }
+            price = _price;
+        }
+
+        //Методы Get
+        public string GetText()
+        {
+            return text;
+        }
+
+        public int GetPrice()
+        {
+            return price;
+        }
+
+        public override string ToString()
+        {
+            return text + " " + price; 
+        }
+
+
+    }
+
+    class ProperList 
+    {
+        string name;
+        private List<Proper> list;
+
+        public ProperList(string _name, List<Proper> propers)
+        {
+            name = _name;
+            list = propers;
+        }
+
+        //интерфейс взаимодействия с классом
+        public List<Proper> GetPropers()
+        {
+            return list;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+    }
+
+    internal class Data
+    {
+        static public string resultString="";
+        static public int resultPrice=0;
+
+        static public List<ProperList> properList;
+        static public void Init()
+        {
+            ProperList properList0 = new ProperList("Форма торта", new List<Proper>() { new Proper("Круг", 500),
+                                                                   new Proper("Квадрат", 500),
+                                                                   new Proper("Прямоугольник", 500),
+                                                                   new Proper("Сердечко", 500)});
+            ProperList properList1 = new ProperList("Размер торта", new List<Proper>() { new Proper("маленький", 500),
+                                                                    new Proper("средний", 1000),
+                                                                    new Proper("большой", 2000),
+                                                                    new Proper("особый", 5000)});
+            ProperList properList2 = new ProperList("Вкус коржей", new List<Proper>() { new Proper("ванильный", 300),
+                                                                    new Proper("шоколадный", 400),
+                                                                    new Proper("карамельный", 400),
+                                                                    new Proper("ягодный", 500)});
+            ProperList properList3 = new ProperList("Количество коржей", new List<Proper>() { new Proper("1 корж", 200),
+                                                                    new Proper("2 коржа", 400),
+                                                                    new Proper("3 коржа", 600),
+                                                                    new Proper("4 коржа", 800)});
+            ProperList properList4 = new ProperList("Глазурь", new List<Proper>() { new Proper("шоколад", 100),
+                                                                    new Proper("крем", 200),
+                                                                    new Proper("драже", 150),
+                                                                    new Proper("ягоды", 400)});
+            ProperList properList5 = new ProperList("Декор", new List<Proper>() { new Proper("шоколадная", 300),
+                                                                    new Proper("ягодная", 600),
+                                                                    new Proper("кремовая", 400),
+                                                                    new Proper("карамельная", 500)});
+            properList = new List<ProperList>();
+            properList.Add(properList0);
+            properList.Add(properList1);
+            properList.Add(properList2);
+            properList.Add(properList3);
+            properList.Add(properList4);
+            properList.Add(properList5);
+        }
+
+    }
+}
---
+++ Prakt05/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prakt05
+{
+    class Menu
+    {
+
+        static void ShowArrow(int position,int shiftY)
+        {
+            Console.SetCursorPosition(0, position +shiftY);
+            Console.WriteLine("->");
+        }
+
+ 
+        static public void ShowMenu1()
+        {
+            int position = 0;
+            ConsoleKeyInfo key;
+            do
+            {
+                Console.Clear();
+
+                Console.WriteLine("");
+                Console.WriteLine("Выберите пункт меню");
+                Console.WriteLine("----------------------");
+
+                for (int i = 0; i < Data.properList.Count; i++)
+                    Console.WriteLine("   " + Data.properList[i].GetName());
+                Console.WriteLine("Цена:" + Data.resultPrice);
+                Console.WriteLine(Data.resultString);
+                ShowArrow(position, 3);
+
+                key = Console.ReadKey();
+                if (key.Key == ConsoleKey.UpArrow)
+                {
+                    if (position > 0)
+                        position--;
+                }
+                if (key.Key == ConsoleKey.DownArrow)
+                {
+                    if (position < Data.properList.Count - 1)
+                        position++;
+                }
+                if (key.Key == ConsoleKey.Enter)
+                {
+                    
+                    ShowMenu2(position);
+                }
+                if (key.Key == ConsoleKey.Escape)
+                    break;
+            }
+            while (true);
+            Console.WriteLine("Good bye!");
+        }
+
+        static public void ShowMenu2(int propN)
+        {
+            int position = 0;
+            ConsoleKeyInfo key;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Добро пожаловать в кондитерскую");
+                Console.WriteLine("Выберите пункт меню для заказа");
+                Console.WriteLine("----------------------");
+
+                for (int i = 0; i < Data.properList[propN].GetPropers().Count; i++)
+                Console.WriteLine("   " + Data.properList[propN].GetPropers()[i]);
+                Console.WriteLine("Цена:"+Data.resultPrice);
+                Console.WriteLine(Data.resultString);
+                ShowArrow(position, 3);
+
+                key = Console.ReadKey();
+                if (key.Key == ConsoleKey.UpArrow)
+                {
+                    if (position > 0)
+                        position--;
+                }
+                if (key.Key == ConsoleKey.DownArrow)
+                {
+                    if (position < Data.properList[propN].GetPropers().Count - 1)
+                        position++;
+                }
+                if (key.Key == ConsoleKey.Enter)
+                {
+                    Data.resultString += Data.properList[propN].GetPropers()[position].ToString()+";";
+                    Data.resultPrice += Data.properList[propN].GetPropers()[position].GetPrice();
+                }
+                if (key.Key == ConsoleKey.Escape)
+                    return;                
+            }
+            while (true);
+            
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string yn = "";
+            Data.Init();
+            do
+            {
+                Data.resultPrice = 0;
+                Data.resultString = "";
+                Menu.ShowMenu1();
+                Console.Clear();
+                string text = "Заказ от <" + DateTime.Now.ToShortDateString() + ">\nЗаказ:<" + Data.resultString + ">\nЦена:<" + Data.resultPrice + ">";
+                Console.WriteLine(text);
+                System.IO.File.AppendAllText("zakaz.txt", text);
+                Console.WriteLine("Data saved");
+                Console.WriteLine("Сделать еще один заказ(Yes,No)?");
+                yn=Console.ReadLine();
+
+            }
+            while (Char.ToLower(yn[0]) == 'y');
+
+            
+        }
+    }
+}
